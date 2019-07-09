d9ad28fb5db7c74b0fd29eea42d74162eb62ab81
Alexei Pesic <alexei@liftoff.io>
2019-07-09 14:48:26 -0700

    Rename files

--- config.sh
+++ config.sh
@@ -20,20 +20,20 @@ SCRIPTDIR=$(cd `dirname $0` && pwd)
 
 # Create Links
 cd ~
-ln -s $SCRIPTDIR/.aliases .aliases
-ln -s $SCRIPTDIR/.gitconfig .gitconfig
-ln -s $SCRIPTDIR/.spacemacs .spacemacs
-ln -s $SCRIPTDIR/.zshenv .zshenv
-ln -s $SCRIPTDIR/.zshrc .zshrc
-ln -s $SCRIPTDIR/.hammerspoon .hammerspoon
-ln -s $SCRIPTDIR/.psqlrc .psqlrc
-ln -s $SCRIPTDIR/.vim .vim
-ln -s $SCRIPTDIR/.iterm2 .iterm2
-ln -s $SCRIPTDIR/.lein .lein
-ln -s $SCRIPTDIR/.zprezto .zprezto
-ln -s $SCRIPTDIR/.zpreztorc .zpreztorc
-ln -s $SCRIPTDIR/.zlogin .zlogin
-ln -s $SCRIPTDIR/.zlogout .zlogout
-ln -s $SCRIPTDIR/.zprofile .zprofile
-ln -s $SCRIPTDIR/.ideavimrc .ideavimrc
+ln -s $SCRIPTDIR/aliases .aliases
+ln -s $SCRIPTDIR/gitconfig .gitconfig
+ln -s $SCRIPTDIR/spacemacs .spacemacs
+ln -s $SCRIPTDIR/zshenv .zshenv
+ln -s $SCRIPTDIR/zshrc .zshrc
+ln -s $SCRIPTDIR/hammerspoon .hammerspoon
+ln -s $SCRIPTDIR/psqlrc .psqlrc
+ln -s $SCRIPTDIR/vim .vim
+ln -s $SCRIPTDIR/iterm2 .iterm2
+ln -s $SCRIPTDIR/lein .lein
+ln -s $SCRIPTDIR/zprezto .zprezto
+ln -s $SCRIPTDIR/zpreztorc .zpreztorc
+ln -s $SCRIPTDIR/zlogin .zlogin
+ln -s $SCRIPTDIR/zlogout .zlogout
+ln -s $SCRIPTDIR/zprofile .zprofile
+ln -s $SCRIPTDIR/ideavimrc .ideavimrc
 ln -s $SCRIPTDIR/karabiner .config/karabiner
